ff48b1d44d607b0516d3a857b5719e7335efd6b5
Bigem Maharjan <bmaharjan2906@conestogac.on.ca>
2024-08-09 20:12:48 -0400

    Add Product done and Admin Category

--- frontend_sb_jewellers/src/pages/admin/AddProducts.js
+++ frontend_sb_jewellers/src/pages/admin/AddProducts.js
@@ -1,7 +1,221 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function AddProducts() {
-  return <div>AddProducts</div>;
+  // GET Method
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("/api/category")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Fetched category data:", data);
+        setCategories(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching category details:", error);
+      });
+  });
+
+  // Retrieve the token from localStorage
+  const token = localStorage.getItem("token");
+
+  // POST Method
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [category, setCategoryID] = useState("");
+  const [errors, setErrors] = useState({});
+
+  // Function to validate the form
+  const validateForm = () => {
+    const newErrors = {};
+
+    // Validate name
+    if (!name) {
+      newErrors.name = "Name is required";
+    }
+
+    // Validate Price
+    if (!price) {
+      newErrors.price = "Price is required";
+    }
+
+    // Validate Description
+    if (!description) {
+      newErrors.description = "Description is required";
+    }
+
+    // Validate Quantity
+    if (!quantity) {
+      newErrors.quantity = "Quantity is required";
+    }
+
+    // Validate Image Url
+    if (!imageUrl) {
+      newErrors.imageUrl = "ImageUrl is required";
+    }
+
+    setErrors(newErrors);
+
+    // Return true if there are no errors
+    return Object.keys(newErrors).length === 0;
+  };
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate the form before submitting
+    if (!validateForm()) {
+      return;
+    }
+
+    // Create an object with the name, email, and password
+    const addNewProductData = {
+      name: name,
+      price: price,
+      description: description,
+      quantity: quantity,
+      imageUrl: imageUrl,
+      category: category,
+    };
+    try {
+      // Send a POST request to the backend
+      const response = await fetch("http://localhost:5000/api/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // Include the token here
+        },
+        body: JSON.stringify(addNewProductData),
+      });
+
+      if (response.ok) {
+        // Handle successful response
+        const data = await response.json();
+        console.log("Product Added Successfully:", data);
+
+        // Show an alert message
+        alert("Product Add successful!");
+        // Clear the form fields
+        setName("");
+        setPrice("");
+        setDescription("");
+        setQuantity("");
+        setImageUrl("");
+        setCategoryID("");
+        setErrors({});
+      } else {
+        // Handle error response
+        console.error("Product Add failed:", response.statusText);
+        alert("Product Add failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again later.");
+    }
+  };
+
+  return (
+    <main>
+      <section className="contactForm">
+        <form onSubmit={handleSubmit}>
+          <h1>Add New Page</h1>
+
+          {/* Name */}
+          <div className="input-control">
+            <label htmlFor="name">Name*</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {errors.name && <p className="error">{errors.name}</p>}
+          </div>
+
+          <div className="input-control">
+            <label htmlFor="price">Price*</label>
+            <input
+              id="price"
+              type="number"
+              name="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            {errors.price && <p className="error">{errors.price}</p>}
+          </div>
+
+          <div className="input-control">
+            <label htmlFor="description">Description*</label>
+            <input
+              id="description"
+              type="text"
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            {errors.price && <p className="error">{errors.price}</p>}
+          </div>
+
+          <div className="input-control">
+            <label htmlFor="quantity">Quantity*</label>
+            <input
+              id="quantity"
+              type="number"
+              name="quantity"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+            {errors.quantity && <p className="error">{errors.quantity}</p>}
+          </div>
+
+          {/* Image */}
+          <div className="input-control">
+            <label htmlFor="imageUrl">ImageUrl*</label>
+            <input
+              id="imageUrl"
+              type="text"
+              name="imageUrl"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+            {errors.imageUrl && <p className="error">{errors.imageUrl}</p>}
+          </div>
+
+          {/* Category DropDown */}
+          <div className="input-control">
+            <label htmlFor="dropdown">Select a Category:</label>
+            <select
+              id="dropdown"
+              value={category}
+              onChange={(e) => setCategoryID(e.target.value)}
+            >
+              <option value="">--Select a Category--</option>
+              {categories.map((categoryData) => (
+                <option key={categoryData._id} value={categoryData._id}>
+                  {categoryData.categoryName}
+                </option>
+              ))}
+            </select>
+            {errors.category && <p className="error">{errors.category}</p>}
+          </div>
+
+          {/* Submit Button */}
+          <button className="btn" type="submit">
+            Register
+          </button>
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default AddProducts;
--- frontend_sb_jewellers/src/pages/admin/AdminCategory.js
+++ frontend_sb_jewellers/src/pages/admin/AdminCategory.js
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AdminCategory() {
-  return <div>AdminCategory</div>;
+  const [category, setCategory] = useState([]);
+
+  // Retrieve the token from localStorage
+  const token = localStorage.getItem("token");
+
+  // Initialize navigate
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("/api/category")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Fetched category data:", data);
+        setCategory(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching category details:", error);
+      });
+  });
+
+  //Loader
+  if (!category) {
+    return <div>Loading...</div>;
+  }
+
+  // Add Function
+  const handleAddCategory = () => {
+    // Redirect to the home page
+    navigate("/addCategory");
+  };
+
+  // Update Function
+  const handleUpdateCategory = (id) => {
+    alert(`Update button clicked for category with id ${id}`);
+    // Logic for updating a category goes here
+  };
+
+  // Delete Function
+  const handleDeleteCategory = (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this category?"
+    );
+
+    if (confirmDelete) {
+      fetch(`http://localhost:5000/api/category/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // Include the token here
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to delete the category");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("There was an error deleting the category.");
+        });
+    }
+  };
+
+  return (
+    <div className="product-table-container">
+      <div className="top-bar">
+        <h2>Category List</h2>
+        <button onClick={handleAddCategory}>Add New Category</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Image</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {category.map((category) => (
+            <tr key={category._id}>
+              <td>{category.categoryName}</td>
+              <td>{category.categoryDesc}</td>
+              <td>
+                <img
+                  src={category.imageUrl}
+                  alt={category.CategoryName}
+                  className="product-image"
+                />
+              </td>
+              <td>
+                <button
+                  className="update-btn"
+                  onClick={() => handleUpdateCategory(category._id)}
+                >
+                  Update
+                </button>
+
+                <button
+                  className="delete-btn"
+                  onClick={() => handleDeleteCategory(category._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default AdminCategory;
